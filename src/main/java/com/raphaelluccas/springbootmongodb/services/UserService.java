2f579c339ebad33d1ece6e329bed95123cc7edef
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-10-22 10:15:51 -0300

    Conectando ao MongoDb com repository e service

---
+++ src/main/java/com/raphaelluccas/springbootmongodb/services/UserService.java
@@ -0,0 +1,20 @@
+package com.raphaelluccas.springbootmongodb.services;
+
+import com.raphaelluccas.springbootmongodb.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+
+    }
+}
