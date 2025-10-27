8195cc712b28669d176badd569c9769f97ecd5f7
RaphaelLuccas <raphaelluccasdev@gmail.com>
2025-10-27 11:50:29 -0300

    Consulta simples com @Query

--- src/main/java/com/raphaelluccas/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/raphaelluccas/springbootmongodb/repository/PostRepository.java
@@ -4,12 +4,16 @@ package com.raphaelluccas.springbootmongodb.repository;
 import com.raphaelluccas.springbootmongodb.domain.Post;
 import com.raphaelluccas.springbootmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title':  { $regex:  ?0, $options:  'i'} }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
